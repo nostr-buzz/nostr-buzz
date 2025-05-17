59de3e5ee0fbc47ab0bb128da5b6d131ac032e08
Milad Raeisi <miladsoft@yahoo.com>
2025-05-18 01:54:13 +0400

    feat: create UI components barrel file to streamline exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,22 @@
+// UI components barrel file - exports all UI components from a single entry point
+
+// Export button component
+export * from './button';
+
+// Export card components
+export * from './card';
+
+// Export dialog components
+export * from './dialog';
+
+// Export form components
+export * from './input';
+export * from './label';
+export * from './textarea';
+
+// Export table components
+export * from './table';
+
+// Export other components as needed
+// If a component file doesn't exist yet, you'll need to create it first
+// before adding it to this barrel file
